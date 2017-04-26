f0259933609284be1b90f8cc75ac280e3ac76349
Vishnu Kiran <vishnu.lp@gmail.com>
2017-04-26 09:03:34 +0530

    commit

--- musical/api/urls.py
+++ musical/api/urls.py
@@ -14,7 +14,7 @@ from .views import (GenreListAPI,
 urlpatterns = [
     url(r'^(?P<pk>\d+)/edit$',GenreUpdateAPI.as_view(),name='api_update'),
     url(r'^(?P<pk>\d+)/$',GenreDetailAPI.as_view(),name='api_detail'),
-    url(r'^create/$',GenreCreateAPI.as_view(),name='create'),
+    url(r'^create/$',GenreCreateAPI.as_view(),name='api_create'),
     url(r'^(?P<pk>\d+)/delete/$',GenreDestroyAPI.as_view(),name='api_delete'),
     url(r'^$',GenreListAPI.as_view(),name='api_list'),
 
@@ -22,7 +22,7 @@ urlpatterns = [
     url(r'^(?P<pk>\d+)/list$',MusicDetailAPI.as_view(),name='api_music_detail'),
     url(r'^(?P<pk>\d+)/remove/$',MusicDestroyAPI.as_view(),name='api_music_delete'),
     url(r'^show/$',MusicListAPI.as_view(),name='api_music_list'),
-    url(r'^music_create/$',MusicCreateAPI.as_view(),name='music_create')
+    url(r'^music_new/$',MusicCreateAPI.as_view(),name='api_music_create')
 
     # url(r'^music_edit/(?P<id>\d+)/$',music_update,name='music_update'),
     # url(r'^(?P<id>\d+)/$',music_detail,name='music_detail'),
